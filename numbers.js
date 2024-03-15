d1a44c3d2a2becac66b7e7cdc911d1d7a7839ab5
richardid461 <163119201+richardid461@users.noreply.github.com>
2024-03-15 03:02:13 +0100

    Create numbers.js

---
+++ numbers.js
@@ -0,0 +1,29 @@
+// Example of working with numbers in JavaScript
+
+// Creating variables to store numbers
+var number1 = 10;
+var number2 = 5;
+
+// Addition
+var sum = number1 + number2;
+console.log("Sum of numbers: " + sum); // Output the result to the console
+
+// Subtraction
+var difference = number1 - number2;
+console.log("Difference of numbers: " + difference); // Output the result to the console
+
+// Multiplication
+var product = number1 * number2;
+console.log("Product of numbers: " + product); // Output the result to the console
+
+// Division
+var quotient = number1 / number2;
+console.log("Quotient of numbers: " + quotient); // Output the result to the console
+
+// Remainder
+var remainder = number1 % number2;
+console.log("Remainder of division of numbers: " + remainder); // Output the result to the console
+
+// Exponentiation
+var power = Math.pow(number1, number2);
+console.log("Number " + number1 + " to the power of " + number2 + " equals: " + power); // Output the result to the console
