61d3e83def8c6fd9e4c682576b7f6f1b3eac8e85
richardid461 <163119201+richardid461@users.noreply.github.com>
2024-04-07 05:09:18 +0200

    Create trim.js

---
+++ trim.js
@@ -0,0 +1,7 @@
+public class StringTrim {
+    public static void main(String[] args) {
+        String str = "  Hello, World!  ";
+        String trimmedStr = str.trim();
+        System.out.println("Trimmed string: " + trimmedStr);
+    }
+}
