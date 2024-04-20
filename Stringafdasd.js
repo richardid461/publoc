0228e961b864818b652c6612050890051cb45e85
richardid461 <163119201+richardid461@users.noreply.github.com>
2024-04-20 04:46:19 +0200

    Create Stringafdasd.js

---
+++ Stringafdasd.js
@@ -0,0 +1,19 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {64, 34, 25, 12, 22, 11, 90};
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Sorted array:");
+        for (int num : array) {
+            System.out.print(num + " ");
+        }
+    }
+}
