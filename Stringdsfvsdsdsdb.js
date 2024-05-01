fd52be942371b10215567633a89354c670f7e757
richardid461 <163119201+richardid461@users.noreply.github.com>
2024-05-01 05:00:03 +0200

    Create Stringdsfvsdsdsdb.js

---
+++ Stringdsfvsdsdsdb.js
@@ -0,0 +1,10 @@
+public class IfElseExample {
+    public static void main(String[] args) {
+        int age = 25;
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+    }
+}
