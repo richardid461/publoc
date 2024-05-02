bb10c19dfc5bafd12926b0e96b1e4d4e70cbe91a
richardid461 <163119201+richardid461@users.noreply.github.com>
2024-05-02 05:44:50 +0200

    Create numbersdvfsdsd.js

---
+++ numbersdvfsdsd.js
@@ -0,0 +1,10 @@
+public class EvenOddCheck {
+    public static void main(String[] args) {
+        int num = 6;
+        if (num % 2 == 0) {
+            System.out.println(num + " is an even number.");
+        } else {
+            System.out.println(num + " is an odd number.");
+        }
+    }
+}
