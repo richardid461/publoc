2025544a15a43c885b23c0dca77b3953629ba809
richardid461 <163119201+richardid461@users.noreply.github.com>
2024-05-15 03:59:33 +0200

    Create numberssdfsd.js

---
+++ numberssdfsd.js
@@ -0,0 +1,21 @@
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.print("Prime numbers up to " + n + ": ");
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1)
+            return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+}
